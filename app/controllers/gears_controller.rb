681aebd646ce2cd138bb7a459a2d29388301bb58
EC2 Default User <ec2-user@ip-172-31-31-158.us-east-2.compute.internal>
2020-10-08 17:27:43 +0000

    Finish 6 action

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -2,13 +2,35 @@ class GearsController < ApplicationController
   def index
     @gears = Gear.all
   end
+  
+  def show
+    @gear = Gear.find(params[:id])
+  end
 
   def new
     @gear = Gear.new
   end
   
   def create
+    @gear = Gear.new(gear_params)
+      if @gear.save
+        flash[:notice] = "ギア登録が完了しました"
+        redirect_to gears_path
+      else
+        flash.now[:alert] = "必須項目を入力してください"
+        render "/gears/new"
+      end
+  end
+  
+  def destroy
+    @gear = Gear.find(params[:id])
+    @gear.destroy
     redirect_to gears_path
+    flash[:notice] = "ギアを削除しました"
+  end
+  
+  def gear_params
+    params.require(:gear).permit(:category, :name, :maker, :price, :memo)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/about', to: 'static_pages#about'
   devise_for :users
-  get '/gears',to: 'gears#index'
+  
+  get '/gears', to: 'gears#index'
   get 'gears/new', to: 'gears#new'
   post 'gears/create'
+  get 'gears/:id', to: 'gears#show'
+  delete 'gears/:id/destroy', to: 'gears#destroy'
+  
   
 end
