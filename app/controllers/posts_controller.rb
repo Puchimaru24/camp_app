ddfdf54723a89dffd61d0faee49eaaafff39863c
EC2 Default User <ec2-user@ip-172-31-31-158.us-east-2.compute.internal>
2020-10-14 17:52:58 +0000

    Add avatar to Index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   
   def create
     @post = Post.new(content: params[:content])
+    @post.user_id = current_user.id
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to(posts_path)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140}}
-  # validates :user_id, {presence: true}
-  
-  # mount_uploader :image, ImageUploader
+  validates :user_id, {presence: true}
     
+  has_one_attached :avatar
 end
