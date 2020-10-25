91e7fd610414c4f1a07c0273f1a21f0647a62fe4
EC2 Default User <ec2-user@ip-172-31-31-158.us-east-2.compute.internal>
2020-10-25 07:48:16 +0000

    strageをlocalへ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 end
