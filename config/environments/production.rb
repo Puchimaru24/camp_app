278f7bace8605457f1a3621dd3f0d8838bd36d7e
Puchimaru24 <yamato.ook@gmail.com>
2020-10-28 19:53:57 +0900

    Finish Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
   
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+
+
+  config.assets.initialize_on_precompile = false
 end
