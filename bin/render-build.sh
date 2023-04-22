4622ffb49d64b1d9ab93ed2c25184dd6045eb7b8
YAMATO-DESKTOP <yamato.ook@gmail.com>
2023-04-23 00:05:33 +0900

    本番環境DBをpostgresに変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,4 @@
 set -o errexit
 
 bundle install
-yarn install
-yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec raild db:migrate # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
